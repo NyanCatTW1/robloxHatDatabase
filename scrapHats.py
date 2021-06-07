9aa05ac9b4a8634ad5e3b690fb90198d6ab77d9f
Nyan Cat <17372086+e666666@users.noreply.github.com>
2021-06-07 13:20:12 +0800

    No comment

--- scrapHats.py
+++ scrapHats.py
@@ -11,23 +11,34 @@ except Exception:
 
 cursor = None
 
-while True:
-  url = "https://catalog.roblox.com/v1/search/items/details?Category=11&MaxPrice=50&Subcategory=9&SortType=4&Limit=30"
-  if cursor is not None:
-    url += "&cursor={}".format(cursor)
-  response = requests.get(url).content
-  data = json.loads(response)
+for sortType in [0, 1, 2, 3, 4, 5]:
+  for genre in [-1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15]:
+    print(sortType, genre)
+    while True:
+      url = "https://catalog.roblox.com/v1/search/items/details?Category=11&MaxPrice=50&Subcategory=9&Limit=30"
 
-  for hat in data["data"]:
-    if str(hat["id"]) not in extracted:
-      extracted[str(hat["id"])] = {"name": hat["name"], "price": hat["price"]}
-      print(extracted[str(hat["id"])])
-      keepGoing = True
+      if not sortType == -1:
+        url += "&SortType={}".format(sortType)
 
-  print(len(extracted))
-  cursor = data["nextPageCursor"]
-  if cursor is None:
-    break
+      if not genre == -1:
+        url += "&Genres={}".format(genre)
+
+      if cursor is not None:
+        url += "&cursor={}".format(cursor)
+
+      response = requests.get(url).content
+      data = json.loads(response)
+
+      for hat in data["data"]:
+        if str(hat["id"]) not in extracted:
+          extracted[str(hat["id"])] = {"name": hat["name"], "price": hat["price"]}
+          print(extracted[str(hat["id"])])
+          keepGoing = True
+
+      print(len(extracted))
+      cursor = data["nextPageCursor"]
+      if cursor is None:
+        break
 
 with open("hats.json", "w") as f:
   json.dump(extracted, f, sort_keys=True, indent=2)
